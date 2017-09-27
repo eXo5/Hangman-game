ea900e608c31f3d2b2002d805c0ddad6c7a43725
eXo5 <mike.yingling15@gmail.com>
2017-09-26 22:57:32 -0400

    redoing this entirely - currently not counting incorrect guesses properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,9 @@
 	var wordList = ["lighthouse", "boardwalk", "beach", "sand", "waves", "seagull", "seashell", "shark", "dolphin"];
 	//$ not defined = no jQuery 
 	//declared variables
-	var randomWord = wordList[Math.floor(Math.random() * wordList.length)]; //generate word to be guessed from list
-	var wordlength = randomWord.length //redundant  
+	 //generate word to be guessed from list
+	var randomWord;
+	//var wordlength = randomWord.length //redundant  
 	var guessesArr = []; //userKey guesses pushed here for comparison
 	var wordArr = randomWord.split(""); //generates strings of individual letters from randomWord
 	var underSarray = []; //array of underscores == wordArr
@@ -12,38 +13,44 @@
 	var guess = document.querySelector("#guess");
 	var guessLeft = document.querySelector("#guessesLeft");
 	var g1 = document.querySelector("#guesses");
-	var guesses = 7; //use document.querySelector to html guesses into the span id=guessesLeft.
+	 //use document.querySelector to html guesses into the span id=guessesLeft.
 	//var document.querySelector("#guesses");
 	var lettersArr = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	var answer = document.querySelector("#answer");
-
+	var guesses = 8;
 //this function doesn't work as intended, but it was supposed to start a new word =\
 	
 
 		function checkGuess(userKey){
+			
+			
+		
 			if (lettersArr.indexOf(userKey) == -1){ 
 				alert("Please choose a letter");
 			}
 					//if not in word and not in guessArr
-				if ((wordArr.indexOf(userKey) == -1) && (guessesArr.indexOf(userKey) == -1)) {
-					guessesArr.push(userKey);
-					--guesses;
-					guessLeft.textContent = (guesses + " guesses left."); 
-				}
-			else {
-					for (var i = 0; i < wordArr.length; i++)
-					 	if (userKey == wordArr[i]) {
-					 	//var x = wordArr.indexOf(userKey);
-						underSarray[i] = userKey;
-						
+			if ((wordArr.indexOf(userKey) == -1) && (guessesArr.indexOf(userKey) == -1)) {
+				guessesArr.push(userKey);
+				--guesses;
+				guessLeft.textContent = (guesses + " guesses left."); 
+			} else {
+				var wordArr = randomWord.split("");
+				for (var i = 0; i < wordArr.length; i++)
+				 	if (userKey == wordArr[i]) {
+				 	//var x = wordArr.indexOf(userKey);
+					underSarray[i] = userKey;
+					
 
-						//x = wordArr.indexOf(userKey, x+1);
-						answer.innerHTML = underSarray.join(" ");
-					}
+					//x = wordArr.indexOf(userKey, x+1);
+					answer.innerHTML = underSarray.join(" ");
 				}
+			}
+
 		 };
 		 //insUndScore takes random word and pushes underscores onto span #answer
 		function insUndScore(randomWord) {
+			
+			randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 			for (var i = 0;  i < randomWord.length; i++) { 
 				underSarray.push("_");
 				answer.innerHTML = underSarray.join(" ");
@@ -57,11 +64,13 @@
 		console.log("randomword:" + randomWord);
 		insUndScore(randomWord);
 
-	function game(guesses) {
-		var losses = x;
-		if (guesses = 0);
-			insUndScore(randomWord);
+	function resetGame() {
+		if (guesses === 0) {
+		guesses = 8;
+		underSarray = [];
+		insUndScore(randomWord);
 		alert("Please Try Again");
+		}
 	};
 	//userKey = guess a letter && push guess into array for guesses.
 	document.onkeyup = function(event) {
@@ -69,9 +78,11 @@
 		guess.textContent = " " + userKey;
 		g1.textContent = guessesArr.join(" ");
 		checkGuess(userKey)
+		resetGame();
 		console.log("guessesArr: " + guessesArr);
 		
 
-		}
+	}
 
+	
 		//game(guesses);
---
+++ assets/js/hangman.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+	//$("#hangman").html("<div><h1>HANGMAN</h1></div>")
+})
+	//our metaphorical toolbox
+	var toolbox = ["hammer", "drill", "nails", "screws", "flathead screwdriver"];
+	//where guesses get stored after they're clicked
+	var guesses = [];
+	var pickAWordAnyWord;
+	pickAWordAnyWord = toolbox[Math.floor(Math.random() * toolbox.length)]
+	//.split() is an inherent function in javascript that 'splits' a string into individual words or letters depending on the separator you use.
+	var pickedWord = pickAWordAnyWord.split("");
+	//Effectively, we'll use this to modify the pickedWord.
+	var wordToGuess;
+	var guessesLeft;
+	//console.log the word for testing purposes that way you're not playing hangman while you're working on it haha
+	console.log(pickAWordAnyWord)
+	//Start the game by clicking "Play A Game" button
+$("button").click(function(){
+	$("#wordClue").html("<br /><p style='text-align:center'>This word can be found in a toolbox</p>")
+	//declare vars
+
+	guessesLeft = 7;
+
+	//alert(pickAWordAnyWord)
+	//alert(toolbox[Math.floor(Math.random()* toolbox.length)])
+	wordToGuess = [];
+		for (var i = 0; i < pickAWordAnyWord.length; i++){
+			wordToGuess.push("_")
+		}
+		console.log(wordToGuess)
+	$("#word").html("<p style='text-align:center'>" + wordToGuess.join(" ") + "</p>");
+	$("#pickALetter").html("<p style='text-align:center'>Press a key to guess a letter!</p>");
+})
+	
+$(document).keyup(function(event) {
+	var userKey = event.key;
+console.log(pickedWord)
+	if (event.keyCode >= 65 && event.keyCode <= 90){
+  	//check if the event that occurred was a letter 
+		if (guesses.indexOf(userKey) == -1) {
+			//check if the letter has been guessed before. 
+			guesses.push(userKey);
+			//then nest an if statement inside of an if statement to check if the key that hasn't been guessed before now is in the pickAWordAnyWord
+			if (pickedWord.indexOf(userKey) > -1 ){
+				var whereKey = pickedWord.indexOf(userKey); console.log(whereKey);
+				//wordToGuess.splice(whereKey, 1, userKey); console.log(wordToGuess);
+				//.map is what's called a higher level function. It's similar to the javascript built in methods, but it has predefined arguments that are required for functionality.
+				pickedWord.map(function(element, i){
+					if (element == userKey){
+						wordToGuess[i] = userKey;
+					}
+				})
+			 	$("#word").html("<p style='text-align:center'>" + wordToGuess.join(" ") + "</p>");
+			}
+		}	else if (guesses.indexOf(userKey) > -1 ) {
+			alert("You already guessed that")
+		} else if (pickedWord.indexOf(userKey) == -1){
+			guessesLeft--;
+			console.log(guessesLeft)
+		}
+	}//end if event.keyCode
+	
+		console.log(guesses); 
+		$("#guessedLetters").html("<p style='text-align:center'>Guesses: " + guesses.join(" ") + "</p>")
+	
+	
+})
