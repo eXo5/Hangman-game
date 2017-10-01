16a109f68f5e909ac80d5fc5849ac66187d734cb
eXo5 <mike.yingling15@gmail.com>
2017-10-01 16:42:35 -0400

    it's coming together more, but I still can't win or lose and reset a new word

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,51 +1,39 @@
-$(document).ready(function(){
-	var guessesLeft;
-	//$("#hangman").html("<div><h1>HANGMAN</h1></div>")
-})
-	//our metaphorical toolbox
+var pickedWord;
+var guessesLeft;
+var guesses;
+var wordToGuess;
+var pickAWordAnyWord;
+$(document).on("click", "button", function(){
+	console.log("button click")
+		//our metaphorical toolbox
 	var toolbox = ["hammer", "drill", "nails", "screws", "flathead screwdriver"];
-	//where guesses get stored after they're clicked
-	var guesses = [];
-	var pickAWordAnyWord;
-	pickAWordAnyWord = toolbox[Math.floor(Math.random() * toolbox.length)]
-	//.split() is an inherent function in javascript that 'splits' a string into individual words or letters depending on the separator you use.
-	var pickedWord = pickAWordAnyWord.split("");
-	//Effectively, we'll use this to modify the pickedWord.
-	var wordToGuess;
-
-	//console.log the word for testing purposes that way you're not playing hangman while you're working on it haha
-	console.log(pickAWordAnyWord)
-	//Start the game by clicking "Play A Game" button
-$("button").click(function(){
-	$("#wordClue").html("<br /><p style='text-align:center'>This word can be found in a toolbox</p>")
+	pickAWordAnyWord = toolbox[Math.floor(Math.random() * toolbox.length)];
+	pickedWord = pickAWordAnyWord.split("");
 	//declare vars
-
-guessesLeft = 7;
-
-	//alert(pickAWordAnyWord)
-	//alert(toolbox[Math.floor(Math.random()* toolbox.length)])
+	guessesLeft = 7;
 	wordToGuess = [];
+	guesses = [];
 		for (var i = 0; i < pickAWordAnyWord.length; i++){
 			wordToGuess.push("_")
 		}
 		console.log(wordToGuess)
+
+	$("#wordClue").html("<br /><p style='text-align:center'>This word can be found in a toolbox</p>")
 	$("#word").html("<p style='text-align:center'>" + wordToGuess.join(" ") + "</p>");
 	$("#pickALetter").html("<p style='text-align:center'>Press a key to guess a letter!</p>");
-})
+})//end button click
 	
 $(document).keyup(function(event) {
 	var userKey = event.key;
-		console.log(pickedWord)
-		if (guessesLeft > 0){
-			$("#guessesLeft").html("You have " + guessesLeft + " guesses left!")
-		
+	//console.log(pickedWord)
+if (guessesLeft > 0) {
 	if (event.keyCode >= 65 && event.keyCode <= 90){
   	//check if the event that occurred was a letter 
 		if (guesses.indexOf(userKey) == -1) {
 			//check if the letter has been guessed before. 
 			guesses.push(userKey);
-			//then nest an if statement inside of an if statement to check if the key that hasn't been guessed before now is in the pickAWordAnyWord
 			if (pickedWord.indexOf(userKey) > -1 ){
+				//then nest an if statement inside of an if statement to check if the key that hasn't been guessed before now is in the pickAWordAnyWord by verifying the value returned from the index search is greater than -1(it will return the position value of the index of the letter)
 				var whereKey = pickedWord.indexOf(userKey); console.log(whereKey);
 				//wordToGuess.splice(whereKey, 1, userKey); console.log(wordToGuess);
 				//.map is what's called a higher level function. It's similar to the javascript built in methods, but it has predefined arguments that are required for functionality.
@@ -55,22 +43,22 @@ $(document).keyup(function(event) {
 					}
 				})
 			 	$("#word").html("<p style='text-align:center'>" + wordToGuess.join(" ") + "</p>");
+			}else {
+				--guessesLeft;
+				$("#guessesLeft").html("<p style='text-align:center'>" + guessesLeft + "<p> guesses left!</p>")
 			}
 		}	else if (guesses.indexOf(userKey) > -1 ) {
 			alert("You already guessed that")
-		} else if (pickedWord.indexOf(userKey) === -1){
-			guessesLeft--;
+		} else if (pickedWord.indexOf(userKey) == -1){
+			--guessesLeft;
 			console.log(guessesLeft)
 		}
-		
+	}//end if event.keyCode
+	
 		console.log(guesses); 
 		$("#guessedLetters").html("<p style='text-align:center'>Guesses: " + guesses.join(" ") + "</p>")
-	}else{
-		alert("Please pick a letter :)")
-	}//end if event.keyCode
-
-	}else if (guessesLeft <= 0){
-			$("#hangman").html("Better luck next time!" + "<button>Play again?</button>")
-		}
+}else{
+	$("#hangman").html("Better Luck Next Time!<br /><button>Play Again?</button>")
+}	
 	
 })
